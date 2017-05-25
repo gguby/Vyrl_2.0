20b181459f2ebe3189bbca60b4e3b014ca1aaf88
KoMyeongbu <kmb@smtown.com>
2017-05-25 16:00:31 +0900

    [kmb] - 로긴뷰 레이아웃 수정 - profiePhotoViewController 추가

---
+++ Vyrl2.0/Login/ProfilePhotoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ProfilePhotoViewController.swift
+//  Vyrl2.0
+//
+//  Created by  KoMyeongbu on 2017. 5. 25..
+//  Copyright © 2017년 smt. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePhotoViewController: UIViewController {
+
+    @IBOutlet weak var fileSizeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        fileSizeLabel.layer.borderColor = UIColor.white.cgColor
+        fileSizeLabel.layer.borderWidth = 1
+        fileSizeLabel.layer.cornerRadius = 9.5
+        fileSizeLabel.layer.masksToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
