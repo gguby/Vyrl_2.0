0782f7ee4a8077c6fed15adbcf902b69d8f98251
wsjung <wsjung@smtown.com>
2017-06-02 15:47:39 +0900

    MY_01_02 화면 수정

--- Vyrl2.0/MY/MyViewController.swift
+++ Vyrl2.0/MY/MyViewController.swift
@@ -33,8 +33,14 @@ class MyViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
     }
 
     @IBAction func showAccountSelectView(_ sender: Any) {
-        dropTableView.isHidden  = false
-        tabBarController?.tabBar.isHidden = true
+        
+        UIView.transition(with: self.dropTableView, duration: 0.5, options: .transitionCrossDissolve, animations: { () -> Void in
+            
+            self.dropTableView.isHidden  = !self.dropTableView.isHidden
+            self.tabBarController?.tabBar.isHidden = !self.dropTableView.isHidden
+            
+        }, completion: nil)
+        
     }
     
     @IBAction func showSetting(){
@@ -76,4 +82,44 @@ class MyAccountCell : UITableViewCell{
     @IBOutlet weak var iconCheck: UIImageView!
     @IBOutlet weak var iconDot: UIImageView!
     
+}
+
+fileprivate let minimumHitArea = CGSize(width: 100, height: 100)
+
+class SmallButton : UIButton {
+    
+}
+
+extension SmallButton {
+    open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        // if the button is hidden/disabled/transparent it can't be hit
+        if self.isHidden || !self.isUserInteractionEnabled || self.alpha < 0.01 { return nil }
+        
+        // increase the hit frame to be at least as big as `minimumHitArea`
+        let buttonSize = self.bounds.size
+        let widthToAdd = max(minimumHitArea.width - buttonSize.width, 0)
+        let heightToAdd = max(minimumHitArea.height - buttonSize.height, 0)
+        let largerFrame = self.bounds.insetBy(dx: -widthToAdd / 2, dy: -heightToAdd / 2)
+        
+        // perform hit test on larger frame
+        return (largerFrame.contains(point)) ? self : nil
+    }
+}
+
+class CustomButton : UIButton {
+    
+}
+
+extension CustomButton {
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted
+            {
+                self.backgroundColor = UIColor(red: 62.0 / 255.0, green: 58.0 / 255.0, blue: 57.0 / 255.0, alpha: 0.3)
+            } else
+            {
+                self.backgroundColor = UIColor.clear
+            }
+        }
+    }
 }
