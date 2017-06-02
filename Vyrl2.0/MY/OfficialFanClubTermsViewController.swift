d2cf19930bd4e80edae984f4e26fa2a87f5508eb
KoMyeongbu <kmb@smtown.com>
2017-06-02 16:30:39 +0900

    [kmb] MY_03_00/10 공식 팬클럽 시버스 이용 약관 스토리 보드 추가

---
+++ Vyrl2.0/MY/OfficialFanClubTermsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  OfficialFanClubTermsViewController.swift
+//  Vyrl2.0
+//
+//  Created by  KoMyeongbu on 2017. 6. 2..
+//  Copyright © 2017년 smt. All rights reserved.
+//
+
+import UIKit
+
+class OfficialFanClubTermsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView(frame: .zero)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int
+    {
+        return 5
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath)
+    {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Instantiate a cell
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)
+        cell.textLabel?.text = "동방신기 공식 팬클럽 이용 약관"
+        
+        return cell;
+    }
+
+}
