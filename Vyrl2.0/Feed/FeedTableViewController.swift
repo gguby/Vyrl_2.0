e46a91dc0be0c91d2ad2b73c323790b385a59649
KoMyeongbu <kmb@smtown.com>
2017-06-09 12:32:05 +0900

    [kmb] 피스 리스트 - 채널 UI 추가

--- Vyrl2.0/Feed/FeedTableViewController.swift
+++ Vyrl2.0/Feed/FeedTableViewController.swift
@@ -19,8 +19,9 @@ class FeedTableViewController: UIViewController, UITableViewDelegate, UITableVie
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.rowHeight = UITableViewAutomaticDimension
-        self.tableView.estimatedRowHeight = 600
+        self.tableView.estimatedRowHeight = 400
         tableView.tableFooterView = UIView(frame: .zero)
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,13 +31,38 @@ class FeedTableViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 4
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return UITableViewAutomaticDimension
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell :FeedTableCell = self.tableView.dequeueReusableCell(withIdentifier: "FeedTableCell") as! FeedTableCell
+        var cell :FeedTableCell = tableView.dequeueReusableCell(withIdentifier: "oneFeed") as! FeedTableCell
+        
+        switch indexPath.row {
+        case 0:
+            cell = tableView.dequeueReusableCell(withIdentifier: "oneFeed") as! FeedTableCell
+            break
+        case 1:
+            cell = tableView.dequeueReusableCell(withIdentifier: "advertisingFeed") as! FeedTableCell
+            break
+        case 2:
+            cell = tableView.dequeueReusableCell(withIdentifier: "textOnlyFeed") as! FeedTableCell
+            break
+        case 3:
+            cell = tableView.dequeueReusableCell(withIdentifier: "multiFeed") as! FeedTableCell
+            break
+        case 4:
+            cell = tableView.dequeueReusableCell(withIdentifier: "channelFeed") as! FeedTableCell
+            break
+        default: break
+        }
+        
         return cell
     }
 
