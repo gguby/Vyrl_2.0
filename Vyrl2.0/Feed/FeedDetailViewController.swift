4a522fe3564b8988a9f5e94c7d4a619ed8c4d696
KoMyeongbu <kmb@smtown.com>
2017-07-07 11:57:59 +0900

    [kmb] 댓글 선택시 죽는 문제 수정

--- Vyrl2.0/Feed/FeedDetailViewController.swift
+++ Vyrl2.0/Feed/FeedDetailViewController.swift
@@ -19,7 +19,6 @@ class FeedDetailViewController: UIViewController,  UITableViewDelegate, UITableV
     @IBOutlet weak var commentFieldView: UIView!
     @IBOutlet weak var buttonView: UIView!
     
-    
     var kbHeight: CGFloat!
 
     override func viewDidLoad() {
@@ -60,16 +59,8 @@ class FeedDetailViewController: UIViewController,  UITableViewDelegate, UITableV
 //            }
 //        }
         
-        let customView = UIView(frame: CGRect(x: 0, y: self.view.frame.size.height-300, width: self.view.frame.size.width, height: 300))
-        customView.backgroundColor = UIColor.red
-        customView.layer.zPosition = CGFloat(MAXFLOAT)
-        customView.tag = 100
+        let keyboard = UIApplication.shared.windows[1]
         
-        let controller : EmoticonViewController = storyboard!.instantiateViewController(withIdentifier: "Emoticon") as! EmoticonViewController
-        customView.addSubview(controller.view)
-        
-        let windowCount = UIApplication.shared.windows.count
-        UIApplication.shared.windows[windowCount-1].addSubview(customView)
         
         
     }
@@ -82,39 +73,50 @@ class FeedDetailViewController: UIViewController,  UITableViewDelegate, UITableV
     }
     
     func keyboardShow(notification: NSNotification) {
-        if let userInfo = notification.userInfo {
-            if let keyboardSize =  (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-                kbHeight = keyboardSize.height
-                self.animateTextField(up: true)
-                let index = IndexPath(row: 9, section: 0)
-                self.tableView.scrollToRow(at: index, at: .bottom, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            let numberOfSections = self.tableView.numberOfSections
+            let numberOfRows = self.tableView.numberOfRows(inSection: numberOfSections-1)
+            
+            if numberOfRows > 0 {
+                let indexPath = IndexPath(row: numberOfRows-1, section: (numberOfSections-1))
+                self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
             }
         }
-//        let endFrame = ((notification as NSNotification).userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-//        bottomConstraint.constant = UIScreen.main.bounds.height - endFrame.origin.y
-//        self.view.layoutIfNeeded()
+
+        
+         if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+            let window = self.view.window?.frame {
+            // We're not just minusing the kb height from the view height because
+            // the view could already have been resized for the keyboard before
+            self.view.frame = CGRect(x: self.view.frame.origin.x,
+                                     y: self.view.frame.origin.y,
+                                     width: self.view.frame.width,
+                                     height: window.origin.y + window.height - keyboardSize.height)
+            
+            
+        } else {
+            debugPrint("We're showing the keyboard and either the keyboard size or window is nil: panic widely.")
+        }
+
     }
     
     func keyboardHide(notification: NSNotification) {
-        self.animateTextField(up: false)
-    }
-    
-    func animateTextField(up: Bool) {
-        var movement = (up ? -kbHeight : kbHeight)
-        
-        UIView.animate(withDuration: 0.3, animations: {
-            self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement!)
-        })
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let viewHeight = self.view.frame.height
+            self.view.frame = CGRect(x: self.view.frame.origin.x,
+                                     y: self.view.frame.origin.y,
+                                     width: self.view.frame.width,
+                                     height: viewHeight + keyboardSize.height)
+        } else {
+            debugPrint("We're about to hide the keyboard and the keyboard size is nil. Now is the rapture.")
+        }
     }
     
     func tapGestureHandler() {
         view.endEditing(true)
+        self.commentFieldView.isHidden = true
+        self.buttonView.isHidden = false
         
-        if let viewWithTag = self.view.viewWithTag(100) {
-            viewWithTag.removeFromSuperview()
-        }else{
-            print("No!")
-        }
     }
     
     override func didReceiveMemoryWarning() {
